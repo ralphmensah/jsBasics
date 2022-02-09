e0a6ec8e095b889f2f9834a47dcf1d47fc1c579e
ralphmensah <ralphmensah123@gmail.com>
2022-02-09 18:54:07 +0000

    weird little summation

--- calc/script.js
+++ calc/script.js
@@ -1,1 +1,29 @@
-document.getElementsById("").innerText;
+document.getElementById("item").innerText;
+console.log("heello")
+
+let myArr = []
+
+function btnClicked(num){
+    myArr.push(num);
+    console.log("clicked " + num)
+    console.log(myArr)
+
+    document.getElementById("item").innerHTML = num
+}
+
+function calculateSum(){
+    let arr = myArr
+    let sum = 0;
+    for (let index = 0; index < arr.length; index++) {
+        const element = arr[index];
+        if (element !== '+'){
+            console.log(element + " -->" + typeof(element))
+            sum = sum + element;
+        }  
+    }
+    console.log(sum)
+    document.getElementById("item").innerHTML = sum
+    myArr = []
+}
+
+
